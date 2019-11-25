de8a4423dd44957e14ae486f6c806b7892b13cdd
Lu-yq <17302010029@fudan.edu.cn>
2019-11-26 00:16:59 +0800

    add the 4-waves agreement

--- project/inc/cmu_tcp.h
+++ project/inc/cmu_tcp.h
@@ -29,6 +29,9 @@ int cmu_write(cmu_socket_t * sock, char* src, int length);
 /*
  * You can add functions below this
  */
- 
+void fdu_initator_disconnect(cmu_socket_t * dst);
+void fdu_listener_disconnect(cmu_socket_t * sock);
+int fdu_free_socket(cmu_socket_t * sock);
+void close_backend(cmu_socket_t * dst);
 
 #endif
--- project/inc/global.h
+++ project/inc/global.h
@@ -17,41 +17,50 @@
 #define TRUE 1
 #define FALSE 0
 
-#define MAX_WND_SIZE 32
-typedef struct {
-  uint32_t buf_len;
-  uint32_t seq;
-  uint32_t ack_waiting_for;
-  uint32_t len;
-  char* msg;
-  int ack_cnt;
-  pthread_mutex_t ack_cnt_lock;
-  clock_t sent_time;
-} pkt_t;
-
-typedef struct {
-  pkt_t* queue[MAX_WND_SIZE];
-  int siz;
-  int front;
-  int next;
-  int end;
- } pkt_window_t;
-
+// about disconnect
+#define CONN_NO_WAIT 0
+#define FIN_WAIT_1 1
+#define FIN_WAIT_2 2
+#define LAST_ACK 3
+#define TIME_WAIT 4
+#define CLOSE_WAIT 5
+#define CLOSED 6
+#define TIMER_ON 1
 
 typedef struct {
 	uint32_t last_seq_received;
 	uint32_t last_ack_received;
-	// the msg is the full packet with header
-	pkt_window_t* send_wnd;
-	// note that msg in recv_wnd is raw data
-	pkt_window_t* recv_wnd;
-
 	pthread_mutex_t ack_lock;
 } window_t;
 
+typedef struct{
+	uint16_t window_size;
+	uint32_t base;
+	uint32_t nextseq;
+	uint32_t estmated_rtt;
+	void** win_packet_buffer;
+	uint16_t buffer_next;
+} sender_t;
+
+typedef struct{
+	uint32_t expect_seq;
+} receiver_t;
+
+typedef struct{
+	struct timeval start_time;
+	struct timeval time_out;
+	uint8_t state;
+} cmu_timer_t;
+
+typedef struct{
+	uint32_t expect_seq;
+	uint32_t expect_ack;
+	uint8_t shakenhands;
+	uint8_t disconnect;
+} connection_t;
 
 typedef struct {
-	int socket;
+	int socket;   
 	pthread_t thread_id;
 	uint16_t my_port;
 	uint16_t their_port;
@@ -67,6 +76,12 @@ typedef struct {
 	int dying;
 	pthread_mutex_t death_lock;
 	window_t window;
+	pthread_mutex_t sender_lock;
+	pthread_mutex_t receiver_lock;
+	sender_t* sender;
+	receiver_t* receiver;
+	cmu_timer_t* timer;
+	connection_t connection;
 } cmu_socket_t;
 
 #endif
